9164ccadcc22a95d19b5d7b6a9ff1d07241256ea
Claij, Angelina <36659310+Angelina-C@users.noreply.github.com>
2024-04-09 15:39:29 +0200

    ADBPY-9: Add inofrmation in setup

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name="adb_py",
     version="0.9",
+    description="adb_py facilitates seamless interaction with Android devices using the Android Debug Bridge (ADB) "
+                "directly within Python scripts.",
+    author="Netherlands Forensic Institute",
+    licence="EUPL-1.2",
     py_modules=["adb_py", "adb_init"],
 )
