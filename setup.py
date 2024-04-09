cffccb307d9f8c445f0ee05a33ff3a35e379cf73
Claij, Angelina <36659310+Angelina-C@users.noreply.github.com>
2024-04-09 16:18:21 +0200

    main: Add long description type and run tests before publishing

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description="adb_py facilitates seamless interaction with Android devices using the Android Debug Bridge (ADB) "
                 "directly within Python scripts.",
     long_description=f"{open('README.md').read()}",
+    long_description_content_type="text/markdown",
     author="Netherlands Forensic Institute",
     author_email="netherlandsforensicinstitute@users.noreply.github.com",
     url="https://github.com/NetherlandsForensicInstitute/adb_py",
