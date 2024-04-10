f9a9d6c581dd39a9aa39ff854d2e2b60f935f47d
Claij, Angelina <36659310+Angelina-C@users.noreply.github.com>
2024-04-10 11:38:33 +0200

    Revert "main: Add long description type and run tests before publishing"
    This reverts commit cffccb307d9f8c445f0ee05a33ff3a35e379cf73.

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from setuptools import setup
 
 setup(
     name="adb_py",
-    version="0.8",
+    version="0.9",
+    description="adb_py facilitates seamless interaction with Android devices using the Android Debug Bridge (ADB) "
+                "directly within Python scripts.",
+    long_description=f"{open('README.md').read()}",
+    author="Netherlands Forensic Institute",
+    author_email="netherlandsforensicinstitute@users.noreply.github.com",
+    url="https://github.com/NetherlandsForensicInstitute/adb_py",
+    licence="EUPL-1.2",
     py_modules=["adb_py", "adb_init"],
+    test_suite="test",
 )
